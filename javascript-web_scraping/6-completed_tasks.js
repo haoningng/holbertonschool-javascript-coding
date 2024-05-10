076018099123c47a0237a55f7f4a84c34d118b70
haoningng <nick.nghn@gmail.com>
2024-05-10 19:24:41 +1000

    feat(task6): add task

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error('code:', response.statusCode);
+  } else {
+    try {
+      const result = {};
+      const data = JSON.parse(body);
+      for (const each of data) {
+        if (each.completed) {
+          result[each.userId] = result[each.userId] ? 
+          result[each.userId] + 1 : 1
+        }
+      }
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
